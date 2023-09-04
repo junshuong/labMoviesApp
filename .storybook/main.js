5417e34a839576c327e871c7933063eb54be5e07
Jun-Shuo Ng <20069422@mail.wit.ie>
2023-09-05 05:14:39 +0800

    "Home page components."

---
+++ .storybook/main.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.js"
+  ],
+
+  "addons": [
+    "@storybook/addon-links",
+  ],
+
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
+  }
+}
