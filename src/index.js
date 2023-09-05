a26f6e0b0fac37bc417b26615fd55fb45345a0fe
Jun-Shuo Ng <20069422@mail.wit.ie>
2023-09-06 00:08:42 +0800

    "Full review page."

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; 
 import {Link} from 'react-router-dom'
+import MovieReviewPage from "./pages/movieReviewPage";
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
       </ul>
       <Routes>
         <Route exact path="/movies/favourites" element={<FavouriteMoviesPage />} />
+        <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
         <Route path="/movies/:id" element={<MoviePage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={ <Navigate to="/" /> } />
