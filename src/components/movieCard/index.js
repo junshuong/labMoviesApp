ff144b562ec7cebf019700bd3df43b4c6c2d7be1
Jun-Shuo Ng <20069422@mail.wit.ie>
2023-09-05 14:30:56 +0800

    "Add to favourites logic completed; Dummy favourites page."

--- src/components/movieCard/index.js
+++ src/components/movieCard/index.js
@@ -12,13 +12,30 @@ import StarRateIcon from "@mui/icons-material/StarRate";
 import IconButton from "@mui/material/IconButton";
 import Grid from "@mui/material/Grid";
 import { Link } from "react-router-dom";
+import Avatar from '@mui/material/Avatar';
 import img from '../../images/film-poster-placeholder.png'
 
 export default function MovieCard(props) {
   const movie = props.movie;
+  const handleAddToFavourite = (e) => {
+    e.preventDefault();
+    props.selectFavourite(movie.id);
+  }
+
   return (
     <Card sx={{ maxWidth: 345 }}>
-      <CardHeader title={movie.title} />
+      <CardHeader avatar={
+          movie.favourite ? (
+            <Avatar sx={{ backgroundColor: 'red' }}>
+              <FavoriteIcon />
+            </Avatar>
+          ) : null
+        }
+        title={
+          <Typography variant="h5" component="p">
+            {movie.title}{" "}
+          </Typography>
+        } />
       <CardMedia
         sx={{ height: 500 }}
         image={
@@ -44,9 +61,9 @@ export default function MovieCard(props) {
         </Grid>
       </CardContent>
       <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites" onClick={null}>
-          <FavoriteIcon color="primary" fontSize="large" />
-        </IconButton>
+      <IconButton aria-label="add to favourites" onClick={handleAddToFavourite}>
+        <FavoriteIcon color="primary" fontSize="large" />
+      </IconButton>
         <Link to={`/movies/${movie.id}`}>
           <Button variant="outlined" size="medium" color="primary">
             More Info ...
