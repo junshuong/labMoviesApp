d2b93137c190a18d88f676ec2330d70fc2b075c2
Jun-Shuo Ng <20069422@mail.wit.ie>
2023-09-06 06:13:07 +0800

    "New button for upcoming movies page."

--- src/pages/upcomingPage.js
+++ src/pages/upcomingPage.js
@@ -1,10 +1,10 @@
 import React from "react";
-
 import { getUpcoming } from "../api/tmdb-api"; // add getUpcoming API hook
 import PageTemplate from '../components/templateMovieListPage';
 import { useQuery } from 'react-query';
 import Spinner from '../components/spinner';
 import AddToFavouritesIcon from '../components/cardIcons/addToFavourites'
+import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd'
 
 const UpcomingPage = (props) => {
   const {  data, error, isLoading, isError }  = useQuery('discover', getUpcoming)
@@ -27,7 +27,8 @@ const UpcomingPage = (props) => {
       title="Upcoming Movies"
       movies={movies}
       action={(movie) => {
-        return <AddToFavouritesIcon movie={movie} />
+        // return <AddToFavouritesIcon movie={movie} />
+        return <PlaylistAddIcon/>
       }}
     />
 );
