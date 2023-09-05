0864b3ab376991040d1fe0bd217cdfddaf1cca41
Jun-Shuo Ng <20069422@mail.wit.ie>
2023-09-06 00:52:32 +0800

    "Added Upcoming movies page."

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import {createRoot} from "react-dom/client";
 import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage";
+import UpcomingPage from "./pages/upcomingPage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; 
 import {Link} from 'react-router-dom'
@@ -12,10 +13,11 @@ const App = () => {
   return (
     <BrowserRouter>
       <SiteHeader />      {/* New Header  */}
-          <Routes>
+      <Routes>
         <Route exact path="/movies/favourites" element={<FavouriteMoviesPage />} />
         <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
         <Route path="/movies/:id" element={<MoviePage />} />
+        <Route path="/movies/upcoming" element={<UpcomingPage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={ <Navigate to="/" /> } />
       </Routes>
