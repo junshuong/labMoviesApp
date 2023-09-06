b83b90de0b662228d862eb2aef60840d95984aa5
Jun-Shuo Ng <20069422@mail.wit.ie>
2023-09-07 06:52:25 +0800

    "Add Top Rated page"
    Unsuccessful - request body issue

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -101,3 +101,17 @@ export const getPopular = () => {
   });
 };
 
+export const getTopRated = () => {
+  return fetch(
+    `https://api.themoviedb.org/3//movie/top_rated?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+  ).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.json().message);
+    }
+    return response.json();
+  })
+  .catch((error) => {
+     throw error
+  });
+};
+
